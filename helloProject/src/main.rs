66659ffaeca0e048f18e7e1a10ddb1ae4c7ba7df
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-10-08 11:01:59 +0100

    Differences and max values of i32 and i64 data types

--- helloProject/src/main.rs
+++ helloProject/src/main.rs
@@ -1,12 +1,20 @@
-// fn main() {
-//     // signed and unsigned integers
-//     // signed can be + or - (i8, i16, i32 i64, i128)
-//     // unsigned can only be + (u8, u16, u32, u64, u128)
-//     let x: i32 = -42
-//     let y: u64 = 100;
-//     println!("Signed Integer: {}", x)
-//     println!("Unsigned Integer: {}", y)
-// }
-fn main(){
-    println!("Hello, 🦀 from CARGO!")
+fn main() {
+    // signed and unsigned integers
+    // signed can be + or - (i8, i16, i32 i64, i128)
+    // unsi   gned can only be + (u8, u16, u32, u64, u128)
+    let x: i32 = -42;
+    let y: u64 = 100;
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+
+    // diff btw i32(32 bits) and i64(64 bits)
+    // range:
+    // i32 range can vary from (-2^31 to +2^31)
+    // i64 range can vary from (-2^63 to +2^63)
+
+    // max values for i32 and i64
+    let e: i32 = 2147483647 ;
+    let i: i64 = 9223372036854775807;
+    println!("Max value of i32: {}", e);
+    println!("Max value of i64: {}", i);
 }
