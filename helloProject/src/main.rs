f61b9a69d43dc3422b85134460b70c0965f4c7a0
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-10-08 10:40:11 +0100

    Rut primitive data types

---
+++ helloProject/src/main.rs
@@ -0,0 +1,12 @@
+// fn main() {
+//     // signed and unsigned integers
+//     // signed can be + or - (i8, i16, i32 i64, i128)
+//     // unsigned can only be + (u8, u16, u32, u64, u128)
+//     let x: i32 = -42
+//     let y: u64 = 100;
+//     println!("Signed Integer: {}", x)
+//     println!("Unsigned Integer: {}", y)
+// }
+fn main(){
+    println!("Hello, 🦀 from CARGO!")
+}
