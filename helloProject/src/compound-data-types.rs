94c97b5da570bb66e95054d73c6c882157019b65
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2026-05-16 03:20:22 +0100

    feat: primitive & compound data types

---
+++ helloProject/src/compound-data-types.rs
@@ -0,0 +1,44 @@
+// Compound Data Types
+// arrays, tuples, slices and strigs(slice strings)
+// Arrays
+fn cmpd_data_types() {
+    // Arrays - homogenous collection of elements of fixed size e.g only int arr, only str arr
+    let numbers: [i32; 5] = [1,2,3,4,5];
+    println!("Number Array: {:?}", numbers);
+    
+    let fruits: [&str; 3] = ["apple", "banana", "orange"];
+    println!("Fruits Arr: {:?}", fruits);
+    println!("Fruits Arr element 1: {}", fruits[0]);
+    println!("Fruits Arr element 2: {}", fruits[1]);
+    println!("Fruits Arr element 3: {}", fruits[2]);
+
+    // Tuples - heterogenous collection of element of a fixed size
+    let human: (String, i32, bool) = ("Alice".to_string(), 30, true);
+    println!("Human tuple: {:?}", human);
+
+    let my_mix_tuple = ("Kratos", 23, true, [1,2,3,5]);
+    println!("My Mix Tuple: {:?}", my_mix_tuple);
+
+    // Slices - dynamically sized 
+    let number_slices: &[i32] = &[1,2,3,4,5];
+    println!("Number Slice: {:?}", number_slices);
+
+    let animal_slices: &[&str] = &["cat", "goat", "dog"];
+    println!("Animal Slice: {:?}", animal_slices);
+
+    let book_slices: &[&String] = &[&"Harry Potter".to_string(), &"Wimpy Kid".to_string(), &"overgeared".to_string()];
+    println!("Book Slice: {:?}", book_slices);
+
+    // Strings vs String Slices (&str) -     
+    // Strings are stored on heap. growable or expandable, you can increase or decrease, hence mutable. You can push and delete data from a certain var if you want. Owned string types.
+    // String slice stored on the stack, can't have mutable data types. not an owned string. is a reference to string stored somewhere in your code. immutable. used to reference string literals or substrings of sting objects without needing to copy or own the data. specfic size
+    // Stack is quicker, heap is slower...stack can't have any mutable dt, heap can have dynamic mutable dt
+    let mut stone_cold: String = String::from("Hell, ");
+    stone_cold.push_str("Yeah!");
+    println!("Stone Cold Says:{}", stone_cold);
+
+    // B- &str ( String Slice)
+    let string: String = String::from("Hello, world!");
+    let slice: &str = &string[0..5];
+    println!("Slice Value: {}", slice);
+}
---
+++ helloProject/src/primitive-data-types.rs
@@ -0,0 +1,34 @@
+fn pdt() {
+    // signed and unsigned integers
+    // signed can be + or - (i8, i16, i32 i64, i128)
+    // unsi   gned can only be + (u8, u16, u32, u64, u128)
+    let x: i32 = -42;
+    let y: u64 = 100;
+    println!("Signed Integer: {}", x);
+    println!("Unsigned Integer: {}", y);
+
+    // diff btw i32(32 bits) and i64(64 bits)
+    // range:
+    // i32 range can vary from (-2^31 to +2^31)
+    // i64 range can vary from (-2^63 to +2^63)
+
+    // max values for i32 and i64
+    let e: i32 = 2147483647 ;
+    let i: i64 = 9223372036854775807;
+    println!("Max value of i32: {}", e);
+    println!("Max value of i64: {}", i);
+
+    // Floats [Floating point types/ fractional integers]
+    // f32, f64
+    let pi: f64 = 3.14;  
+    println!("Value of pi is: {}", pi);
+
+    // Boolean Values true, false
+    let is_snowing: bool = true;
+    println!("Is it snowing: {}", is_snowing);
+
+    // Character type - single character e.g a, z
+    // Note: string is double quote, char is single quote
+    let letter: char = 'a';
+    println!("First letter of the alphabet is: {}", letter)
+}
